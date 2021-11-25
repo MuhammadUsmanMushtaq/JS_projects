8134d0542d4c93b8f77218505959e6c2a4b8e0f6
MuhammadUsmanMushtaq <usman.muhammad@codezyme.com>
2021-11-25 03:13:47 +0100

    logic added

--- Chrome extention/index.js
+++ Chrome extention/index.js
@@ -1,5 +1,19 @@
-let inputBtn = document.querySelector(".input-btn");
+let myLeads = [];
+const inputEl = document.querySelector(".input-el");
+const inputBtn = document.querySelector(".input-btn");
+const ulEl = document.querySelector(".ul-el");
 
 inputBtn.addEventListener("click", function () {
-  console.log("Button Clicked from evenListener");
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li><a href="${myLeads[i]}" target= "_blank">${myLeads[i]}</a></li> `;
+  }
+  ulEl.innerHTML = `${listItems}`;
+}
