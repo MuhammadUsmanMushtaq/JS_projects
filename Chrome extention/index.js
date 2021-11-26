c9ff49a92de09f23e4da5aeb910f680a82f09890
MuhammadUsmanMushtaq <usman.muhammad@codezyme.com>
2021-11-26 02:00:33 +0100

    upload extention into chrome

--- Chrome extention/index.js
+++ Chrome extention/index.js
@@ -3,9 +3,13 @@ const inputEl = document.querySelector(".input-el");
 const inputBtn = document.querySelector(".input-btn");
 const ulEl = document.querySelector(".ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
 
   renderLeads();
 });
@@ -13,7 +17,10 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li><a href="${myLeads[i]}" target= "_blank">${myLeads[i]}</a></li> `;
+    listItems += `
+                <li>
+                  <a href="${myLeads[i]}" target= "_blank">${myLeads[i]}</a>
+                </li> `;
   }
   ulEl.innerHTML = `${listItems}`;
 }
